9e2ba5de20f776d84513f1ff30b05dc68f3f8436
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-07-01 05:23:58 +0530

    delete feature added to important links

--- src/context/importantLinks/ImportantlinkState.js
+++ src/context/importantLinks/ImportantlinkState.js
@@ -28,8 +28,25 @@ const ImportantlinkState = (props) => {
         }
     }
 
+    const deleteImportantlink = async (id) => {
+        try {
+            const client = new Client();
+            const databases = new Databases(client);
+            client
+                .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint   
+                .setProject(process.env.NEXT_PUBLIC_PROJECT_ID) // Your project ID
+
+            const result = await databases.deleteDocument(process.env.NEXT_PUBLIC_DATABASE_ID, process.env.NEXT_PUBLIC_IMPORTANTLINKS_COLLECTION_ID, id);
+
+            fetchImportantlink();
+            return result;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <ImportantlinkContext.Provider value={{ importantlink, setImportantlink, fetchImportantlink }}>
+        <ImportantlinkContext.Provider value={{ importantlink, setImportantlink, fetchImportantlink, deleteImportantlink }}>
             {props.children}
         </ImportantlinkContext.Provider>
     )
