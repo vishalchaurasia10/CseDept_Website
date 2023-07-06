4b334cb5008e4fead9e0134096937871dcea1a92
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-07-07 02:22:42 +0530

    increased speed for industries in mobile mode

--- src/components/Content/Home/Industry.jsx
+++ src/components/Content/Home/Industry.jsx
@@ -7,7 +7,7 @@ const Industry = () => {
             <div className="industryPartners overflow-hidden px-4 mb-16 lg:mb-24 lg:px-40 my-10">
                 <h2 className='text-5xl lg:text-7xl font-jost font-extrabold'>Industry Partners</h2>
                 <p className='text-xl mt-5 mb-8 lg:mb-14 lg:mt-8 font-jost'>The department has MoUs with leading IT companies who have partnered with us to provide state of the art laboratories and learning centres.</p>
-                <div className="slider flex space-x-10 items-center justify-center animate-scrolling">
+                <div className="slider flex space-x-10 items-center justify-center animate-scrollingMobile lg:animate-scrolling">
                     <Image
                         className='w-48'
                         src="/images/industry/sap.png"
--- tailwind.config.js
+++ tailwind.config.js
@@ -33,7 +33,8 @@ module.exports = {
       },
       animation: {
         'up-down': 'up-down 1s ease-in-out infinite',
-        'scrolling': 'scrolling 30s linear infinite',
+        'scrolling': 'scrolling 30s linear -1s infinite',
+        'scrollingMobile': 'scrolling 8s linear -1s infinite',
       },
       backgroundImage: theme => ({
         'hero-gradient': 'radial-gradient(at 54% 40%, hsla(226,72%,93%,1) 0px, transparent 50%), radial-gradient(at 25% 26%, hsla(275,90%,93%,1) 0px, transparent 50%), radial-gradient(at 72% 44%, hsla(183,71%,93%,1) 0px, transparent 50%), radial-gradient(at 87% 59%, hsla(185,61%,93%,1) 0px, transparent 50%), radial-gradient(at 23% 84%, hsla(4,65%,93%,1) 0px, transparent 50%), radial-gradient(at 43% 71%, hsla(321,67%,93%,1) 0px, transparent 50%), radial-gradient(at 91% 4%, hsla(352,77%,93%,1) 0px, transparent 50%)',
