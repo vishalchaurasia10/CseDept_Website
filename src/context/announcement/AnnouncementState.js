b141435044b7a66fa96c77432efdb3003ba48e81
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-09-08 23:12:50 +0530

    separated the upload feature

--- src/context/announcement/AnnouncementState.js
+++ src/context/announcement/AnnouncementState.js
@@ -1,13 +1,95 @@
 import { useContext, useState } from "react";
 import AnnouncementContext from "./announcementContext";
-import { Client, Databases, Storage } from "appwrite";
+import { Client, Databases, ID, Storage } from "appwrite";
 import loadingContext from "../loading/loadingContext";
+import { Toaster, toast } from "react-hot-toast";
 
 const AnnouncementState = (props) => {
 
     const [announcements, setAnnouncement] = useState([])
     const LoadingContext = useContext(loadingContext);
     const { setLoading } = LoadingContext;
+    const failure = (message) => toast.error(message, { duration: 3000 });
+
+    const uploadAnnouncementFile = async (file) => {
+        try {
+            setLoading(true);
+
+            const client = new Client()
+                .setEndpoint('https://cloud.appwrite.io/v1')
+                .setProject(process.env.NEXT_PUBLIC_PROJECT_ID);
+            const storage = new Storage(client);
+
+            const result = await storage.createFile(
+                process.env.NEXT_PUBLIC_ANNOUNCEMENTS_BUCKET_ID,
+                ID.unique(),
+                file
+            );
+
+
+            const fileId = result.$id;
+            const uploadedFile = storage.getFileView(
+                process.env.NEXT_PUBLIC_ANNOUNCEMENTS_BUCKET_ID,
+                fileId
+            );
+
+            toast.promise(
+                Promise.resolve(fileId), // Use `Promise.resolve` to create a resolved promise with the fileId
+                {
+                    success: () => 'Link successfully uploaded!',
+                    error: () => 'Error uploading link.',
+                    duration: 3000,
+                    position: 'top-center',
+                }
+            );
+
+            setLoading(false);
+            return uploadedFile.href;
+
+        } catch (error) {
+            failure('Something went wrong');
+            setLoading(false);
+        }
+    };
+
+    const uploadAnnouncementDocument = async (announcement) => {
+        try {
+            const client = new Client()
+                .setEndpoint('https://cloud.appwrite.io/v1')
+                .setProject(process.env.NEXT_PUBLIC_PROJECT_ID);
+
+            const databases = new Databases(client);
+
+            const formattedDateTime = `${announcement.date} ${announcement.time}:00.000`;
+
+            const result = await databases.createDocument(
+                process.env.NEXT_PUBLIC_DATABASE_ID,
+                process.env.NEXT_PUBLIC_ANNOUNCEMENTS_COLLECTION_ID,
+                ID.unique(),
+                {
+                    title: announcement.title,
+                    description: announcement.description,
+                    venue: announcement.venue,
+                    date: formattedDateTime,
+                    url: announcement.url,
+                },
+            );
+
+            toast.promise(
+                Promise.resolve(result), // Use `Promise.resolve` to create a resolved promise with the fileId
+                {
+                    success: () => 'Announcement successfully uploaded!',
+                    error: () => 'Error uploading announcement.',
+                    duration: 3000,
+                    position: 'top-center',
+                }
+            );
+
+        } catch (error) {
+            failure(error.message);
+        }
+
+    };
 
     const fetchAnnouncements = async () => {
         try {
@@ -63,9 +145,12 @@ const AnnouncementState = (props) => {
     }
 
     return (
-        <AnnouncementContext.Provider value={{ announcements, setAnnouncement, fetchAnnouncements, deleteAnnouncement }}>
-            {props.children}
-        </AnnouncementContext.Provider>
+        <>
+            <Toaster />
+            <AnnouncementContext.Provider value={{ announcements, setAnnouncement, fetchAnnouncements, deleteAnnouncement, uploadAnnouncementFile, uploadAnnouncementDocument }}>
+                {props.children}
+            </AnnouncementContext.Provider>
+        </>
     )
 }
 
