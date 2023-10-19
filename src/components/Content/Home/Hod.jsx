e3520a2cdb72a0731fa1ae59aade2dd2d4e5d587
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-10-20 00:57:13 +0530

    updated hod profile

--- src/components/Content/Home/Hod.jsx
+++ src/components/Content/Home/Hod.jsx
@@ -26,12 +26,12 @@ const Hod = () => {
                 transition={{ duration: 0.5 }}
             >
                 <div className="image lg:w-1/4 mb-6 lg:mb-0 lg:mr-16">
-                    <Image className='w-full rounded-full shadow-2xl shadow-black' src="/images/hod.jpeg" alt="HOD" width={200} height={200} />
+                    <Image className='w-full rounded-full shadow-2xl shadow-black' src="/images/hod.jpg" alt="HOD" width={200} height={200} />
                 </div>
                 <div className="details lg:w-3/4">
-                    <h2 className='text-5xl lg:text-7xl font-jost font-extrabold'>Dr. Annapurna Patil</h2>
+                    <h2 className='text-5xl lg:text-7xl font-jost font-extrabold'>Dr. Anita Kanavalli</h2>
                     <h3 className='text-xl text-justify lg:text-2xl mt-4 font-jost font-extrabold'>Head of the Department, Computer Science & Engineering</h3>
-                    <p className='text-xl mt-5 text-justify lg:mt-8 font-jost'>Dr. Annapurna P. Patil received her Ph.D. in Computer Science and Engineering from VTU in 2014. She has published many research papers in International Conferences and Journals. She is a member of Indian Society of Technical Education (ISTE), IEEE (Computer Society) and CCICI-NIST. She has guided many M.Tech students and is guiding PhD scholars. She is serving the institution from past 20+ years. She is also a IEEE Student Branch Counsellor. Dr. Annapurna P Patil has a wide experience in teaching engineering students both in UG and in PG. She has guided many projects in UG and PG. Her areas of interest include Wireless Networks, Artificial Intelligence, Protocol Engineering, Cloud Computing and IOT, Data Structures and Analysis of Algorithms.</p>
+                    <p className='text-xl mt-5 text-justify lg:mt-8 font-jost'>Dr. Anita Kanavalli is working as Professor & Head of the Department of Computer Science and Engineering of Ramaiah Institute of Technology. Her areas of interest include Adhoc networks, high performance computing and microprocessors.</p>
                 </div>
             </motion.div>
             {/* <span className='h-[0.1rem] ml-4 w-full mx-auto lg:w-[60%] mt-3 bg-black block'></span> */}
